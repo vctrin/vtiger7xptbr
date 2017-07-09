78bbbf2e78af9e6fcbaf8c075422e6880e56ce96
vctrin <valmir@ttcasolucoes.com.br>
2017-07-08 23:39:28 -0300

    Update
    First update

---
+++ modules/Settings/CustomerPortal_Client.php
@@ -0,0 +1,29 @@
+<?php
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is: vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger. All Rights Reserved.
+ * Contributor(s): Valmir C. Trindade - Brazilian Portuguese Translation - www.ttcasolucoes.com.br
+ * ***********************************************************************************/
+
+$languageStrings = Array(
+	'Home' => 'Principal',
+	'More' => 'Mais',
+	'Profile' => 'Perfil',
+	'Change Password' => 'Mudar Senha',
+	'Logout' => 'Desconectar',
+	'Welcome to Portal' => 'Bem Vindo(a) ao Portal',
+	'LBL_CREATE_TICKET' => 'Novo Chamado',
+	'LBL_OPEN_TICKETS' => 'Visualizar Chamados Abertos',
+	'LBL_ADD_DOCUMENT' => 'Carregar um Documento',
+	'What would you like to do ?' => 'O que você gostaria de fazer?',
+	'_start for notify' => 'chaves',
+	'Your support ends on' => 'Seu Suporte termina em',
+	'_start for charts' => 'chaves',
+	'OpenTicketsByPriority' => 'Chamados Abertos por Prioridade',
+	'TicketsClosureTimeByPriority' => 'Tempo de Fechamentos Chamados por Prioridade',
+	'_start for widgets' => 'chaves',
+	'Recent' => 'Recente'
+);
